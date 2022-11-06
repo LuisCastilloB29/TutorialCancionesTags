1a3f432a97d3bdbc6b0849b513a4295eb2b7affc
Luis Castillo <l.castillo1@uniandes.edu.co>
2022-11-05 22:12:34 -0500

    Se agrega una nueva clase de premios

---
+++ src/modelo/premios.py
@@ -0,0 +1,22 @@
+import enum
+
+from sqlalchemy import Column, Integer, String, Enum
+from sqlalchemy.orm import relationship
+
+from .declarative_base import Base
+
+
+class Premios(enum.Enum):
+    LATIN_GRAMI = 1
+    MTV_AWARD = 2
+    GRAMMY = 3
+
+
+class pREMIO(Base):
+    __tablename__ = 'premio'
+
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String)
+    ano = Column(Integer)
+    descripcion = Column(String)
+    medio = Column(Enum(Premios))
